e80e3f671e4b9b81db017327868c13c16f1851b7
ayeshanoor06 <ayeshamirha06@gmail.com>
2026-08-23 09:52:50 +0500

    implemented Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
+
+    implementation(libs.glide)
 }
