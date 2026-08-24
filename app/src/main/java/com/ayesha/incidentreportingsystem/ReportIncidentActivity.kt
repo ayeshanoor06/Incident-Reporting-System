f245ce7ab8c862e2ca3879cc7a673d1b854edc1e
ayeshanoor06 <ayeshamirha06@gmail.com>
2026-08-24 07:59:41 +0500

    Screenshot Added through image picker

--- app/src/main/java/com/ayesha/incidentreportingsystem/ReportIncidentActivity.kt
+++ app/src/main/java/com/ayesha/incidentreportingsystem/ReportIncidentActivity.kt
@@ -1,12 +1,46 @@
 package com.ayesha.incidentreportingsystem
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class ReportIncidentActivity : AppCompatActivity() {
+
+    private lateinit var editTextDescription: EditText
+    private lateinit var imageViewScreenshot: ImageView
+    private lateinit var buttonSelectScreenshot: Button
+    private lateinit var buttonSubmitIncident: Button
+
+    private var selectedImageUri: Uri? = null
+
+    private val imagePickerLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+
+            if (uri != null) {
+                selectedImageUri = uri
+
+                Glide.with(this)
+                    .load(uri)
+                    .into(imageViewScreenshot)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_incident)
+
+        editTextDescription = findViewById(R.id.editTextDescription)
+        imageViewScreenshot = findViewById(R.id.imageViewScreenshot)
+        buttonSelectScreenshot = findViewById(R.id.buttonSelectScreenshot)
+        buttonSubmitIncident = findViewById(R.id.buttonSubmitIncident)
+
+        buttonSelectScreenshot.setOnClickListener {
+            imagePickerLauncher.launch("image/*")
+        }
     }
 }
