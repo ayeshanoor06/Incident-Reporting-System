699d5ced0a163f3da4e7ae34c0759508e0662a30
ayeshanoor06 <ayeshamirha06@gmail.com>
2026-08-24 07:58:50 +0500

    Implemented the Ui of App

--- app/src/main/java/com/ayesha/incidentreportingsystem/MainActivity.kt
+++ app/src/main/java/com/ayesha/incidentreportingsystem/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.ayesha.incidentreportingsystem
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonReportIncident: Button
+    private lateinit var buttonTrackTickets: Button
+    private lateinit var buttonAdminPanel: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        buttonReportIncident = findViewById(R.id.buttonReportIncident)
+        buttonTrackTickets = findViewById(R.id.buttonTrackTickets)
+        buttonAdminPanel = findViewById(R.id.buttonAdminPanel)
+
+        buttonReportIncident.setOnClickListener {
+            val intent = Intent(this, ReportIncidentActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonTrackTickets.setOnClickListener {
+            val intent = Intent(this, TicketTrackingActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonAdminPanel.setOnClickListener {
+            val intent = Intent(this, AdminActivity::class.java)
+            startActivity(intent)
         }
     }
 }
